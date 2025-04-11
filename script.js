f3c80d495c07446fa1869684b4bd8b6712f45962
qqudah <mohannadqqudah@gmail.com>
2025-04-11 11:35:18 +0300

    added certificates section

--- script.js
+++ script.js
@@ -5,3 +5,39 @@ function toggleMenu() {
     icon.classList.toggle("open");
 }
 
+const words = ["cloud", "Back-end"];
+let wordIndex = 0;
+let charIndex = words[0].length;
+let isDeleting = true;
+const changingText = document.getElementById("changing-text");
+
+function typeEffect() {
+  const currentWord = words[wordIndex];
+  const nextWord = words[(wordIndex + 1) % words.length];
+
+  if (isDeleting) {
+    charIndex--;
+    changingText.textContent = currentWord.substring(0, charIndex);
+    
+    if (charIndex === 0) {
+      isDeleting = false;
+      wordIndex = (wordIndex + 1) % words.length;
+    }
+  } else {
+    charIndex++;
+    changingText.textContent = nextWord.substring(0, charIndex);
+    
+    if (charIndex === nextWord.length) {
+      isDeleting = true;
+      setTimeout(typeEffect, 1500); // Pause before deleting again
+      return;
+    }
+  }
+
+  setTimeout(typeEffect, 100);
+}
+
+document.addEventListener("DOMContentLoaded", typeEffect);
+
+
+document.addEventListener("DOMContentLoaded", typeEffect);
